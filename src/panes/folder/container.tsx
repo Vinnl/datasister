214f83b0a2609bcfea71d626dee974f3a3fe3928
Vincent <git@vincenttunru.com>
2019-06-17 21:51:02 +0200

    Use <ResourceLink> in the folder pane as well

--- src/panes/folder/container.tsx
+++ src/panes/folder/container.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { namespaces as ns } from '../../namespace';
 import { PaneContainer } from '../../components/PaneLoader';
+import { ResourceLink } from '../../components/ResourceLink';
+import { NamedNode } from 'rdflib';
 
 export const Container: PaneContainer = (props) => {
   const contents = props.store.statementsMatching(props.resource, ns.ldp('contains'), null, null);
   const cards = contents.map((statement) => {
     return (
       <div className="card">
-        <a
-          href={statement.object.value}
-          onClick={(e) => {e.preventDefault(); props.loadResource(statement.object.value)}}
-          title="View this data"
-        >
-          {statement.object.value}
-        </a>
+        <ResourceLink
+          resource={statement.object as NamedNode}
+          store={props.store}
+          loadResource={props.loadResource}
+        />
       </div>
     );
   });
