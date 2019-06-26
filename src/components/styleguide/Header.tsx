5a0c665eb2bd5845b08f7a73cf2e635471eee8f1
Vincent <git@vincenttunru.com>
2019-06-26 04:22:04 +0200

    Link back to the root from the header

--- src/components/styleguide/Header.tsx
+++ src/components/styleguide/Header.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import $rdf from 'rdflib';
 import Logo from './logo.svg';
 import { ProfileButton } from './ProfileButton';
 import { LoggedIn, LogoutButton, LoggedOut, LoginButton } from '@solid/react';
+import { DataBrowserContext } from '../../context';
+import { usePodOrigin } from '../../hooks/usePodOrigin';
+import { ResourceLink } from '../ResourceLink';
 
 export const Header: React.FC = () => {
+  const { store, fetcher } = React.useContext(DataBrowserContext);
+  const podOrigin = usePodOrigin(store, fetcher);
+
+  const logo = <img src={Logo} style={{height: '25px'}} alt="Data Browser"/>;
+  const logoLink = (podOrigin)
+    ? <ResourceLink resource={$rdf.sym(podOrigin)}>{logo}</ResourceLink>
+    : logo;
+
   return (
     <header className="header fixed">
       <div className="header-wrap">
         <div className="logo-block">
           <h1>
             {/* Unfortunately, the style guide does not display the logo without an explicit height: */}
-            <img src={Logo} style={{height: '25px'}} alt="Data Browser"/>
+            {logoLink}
           </h1>
         </div>
         <nav className="nav nav__toolbar">
