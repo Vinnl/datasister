7c713ea22c92349869a2e2df9161d8c259ec6936
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-17 10:46:52 +0000

    Deploy Vinnl/datasister to github.com/Vinnl/datasister.git:gh-pages

---
+++ precache-manifest.d1a7c5a1858247cd841d500dd346d67c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3287f0fab3e4903e28872fb0aae5dca0",
+    "url": "/datasister/index.html"
+  },
+  {
+    "revision": "d95690369111764ca2fd",
+    "url": "/datasister/static/css/main.5ff4d2ad.chunk.css"
+  },
+  {
+    "revision": "7b59b3468a1a7d72a6b9",
+    "url": "/datasister/static/js/2.ef79a08e.chunk.js"
+  },
+  {
+    "revision": "ea5cbe62a0295ff3d4aa",
+    "url": "/datasister/static/js/3.c90bb9b0.chunk.js"
+  },
+  {
+    "revision": "4d1cf1de01974cea34ab",
+    "url": "/datasister/static/js/4.6bb9df02.chunk.js"
+  },
+  {
+    "revision": "e0df807d55bca068df34",
+    "url": "/datasister/static/js/5.c0b10bb0.chunk.js"
+  },
+  {
+    "revision": "3139be669c5f140ee49a",
+    "url": "/datasister/static/js/6.3d3dba36.chunk.js"
+  },
+  {
+    "revision": "d95690369111764ca2fd",
+    "url": "/datasister/static/js/main.1f84e024.chunk.js"
+  },
+  {
+    "revision": "96ddd61604ba8970a4c2",
+    "url": "/datasister/static/js/runtime~main.9871cf04.js"
+  },
+  {
+    "revision": "13c16db9b1b06302579a72c3b3d6f9e4",
+    "url": "/datasister/static/media/logo.13c16db9.svg"
+  },
+  {
+    "revision": "d34610cf904b652d5ba251e8ab3a3486",
+    "url": "/datasister/static/media/pixolid.d34610cf.svg"
+  },
+  {
+    "revision": "267c6db633482a2342d1588b0d247a37",
+    "url": "/datasister/static/media/user.267c6db6.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/datasister/precache-manifest.ce23b8480b0aef675f76f3730a965bde.js"
+  "/datasister/precache-manifest.d1a7c5a1858247cd841d500dd346d67c.js"
 );
 
 self.addEventListener('message', (event) => {
